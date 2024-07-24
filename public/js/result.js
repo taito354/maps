53fe88f916c675511c3f5d88b6a7d285356405d8
taito2004 <taitototoshi@gmail.com>
2024-07-25 08:40:30 +0900

    amountの大きさに応じてマーカーの大きさを変更する処理を作成

--- public/js/result.js
+++ public/js/result.js
@@ -21,9 +21,41 @@ function initMap(){
 
             const position = new google.maps.LatLng(location.latitude, location.longitude);
 
+
+            //amountの大きさを応じて、ピンのサイズを変更
+            let pinSize = "";
+
+            if(location.amount === "0杯"){
+                pinSize = new google.maps.marker.PinElement({
+                    scale : 0
+                });
+            }else if(location.amount === "~10杯"){
+                pinSize = new google.maps.marker.PinElement({
+                    scale : 0.5
+                });
+            }else if(location.amount === "~100杯"){
+                pinSize = new google.maps.marker.PinElement({
+                    scale : 1.0
+                });
+            }else if(location.amount === "~1,000杯"){
+                pinSize = new google.maps.marker.PinElement({
+                    scale : 1.5
+                });
+            }else if(location.amount === "~10,000杯"){
+                pinSize = new google.maps.marker.PinElement({
+                    scale : 2.0
+                });
+            }else if(location.amount === "爆湧き"){
+                pinSize = new google.maps.marker.PinElement({
+                    scale : 3.0
+                });
+            }
+
+
             new google.maps.marker.AdvancedMarkerElement({
                 position: position,
                 map: mapObj,
+                content: pinSize.element
             });
         });
     };
